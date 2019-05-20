4e6a160e7ec3b12072a72afc6ad9006a8e8021a5
hefang <he529564582@163.com>
2019-05-20 14:53:14 +0800

    add

--- demo2/myapp/routes/index.js
+++ demo2/myapp/routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
 
 module.exports = router;
